e8a3fcf0972e07f1a0d323345919534ceb59a6c4
NICOLAS RIVERA <NICOLAS RIVERA@NRIVERA>
2022-04-27 15:03:18 -0500

    6ta version Se empieza el proceso de metodos, para mejorar la lectura del codigo

--- src/programa1/Programa1.java
+++ src/programa1/Programa1.java
@@ -52,13 +52,12 @@ public class Programa1 {
                 }
 
             } while (first);
-                                     //bucle para que al escoger un no int volver al menu y no generar error
-                if (menu == 1){
+            //bucle para que al escoger un no int volver al menu y no generar error
+            if (menu == 1) {
                 flag = false;            //flag
 
                 try {                        //funcion try catch para generar una excepcion al ecribir una variable no int
 
-                    
                     System.out.println("USTED ESCOGIO OPERACIONES BASICAS \n 1. SUMA \n 2. RESTA \n 3. MULTIPLICACION \n 4. DIVISION");
                     System.out.println("ESCRIBA EL NUMERO DEPENDIENDO LA OPERACION QUE DESEE: ");
 
@@ -68,16 +67,14 @@ public class Programa1 {
                     sc.nextLine();
                     flag = true;                            //si es asi volver a el menu
                 }
-                }
-           
-            
+            }
+
             if (menu == 2) {                          //bucle para que al escoger un no int volver al menu y no generar error
 
                 flag = false;            //flag
 
                 try {                        //funcion try catch para generar una excepcion al ecribir una variable no int
 
-                    
                     System.out.println("USTED ESCOGIO PRIMOS");
                     System.out.println("ESCRIBA EL NUMERO DEPENDIENDO LA OPCION QUE DESEE: \n 1.SECUENCIA DE NUMEROS PRIMOS ");
 
@@ -89,14 +86,13 @@ public class Programa1 {
                 }
 
             }
-            
+
             if (menu == 3) {                          //bucle para que al escoger un no int volver al menu y no generar error
 
                 flag = false;            //flag
 
                 try {                        //funcion try catch para generar una excepcion al ecribir una variable no int
 
-                    
                     System.out.println("USTED ESCOGIO FIBONACCI");
                     System.out.println("ESCRIBA EL NUMERO DEPENDIENDO LA OPCION QUE DESEE \n 1. SECUENCIA FIBONACCI ");
 
@@ -107,9 +103,9 @@ public class Programa1 {
                     flag = true;                            //si es asi volver a el menu
                 }
 
-            } 
+            }
 
-            if (menu2== 1) {
+            if (menu2 == 1) {
                 System.out.println("Usted escogio SUMA");
                 int numero = 0;
                 int numero2 = 0;
@@ -219,4 +215,108 @@ public class Programa1 {
 
     }
 
+    static void suma() {
+        Scanner scan = new Scanner(System.in);
+        int suma1;
+        int suma2;
+        System.out.println("Usted escogio suma");
+        System.out.println("Escriba el primer numero: ");
+        suma1 = scan.nextInt();
+        System.out.println("Escriba el segundo numero");
+        suma2 = scan.nextInt();
+        System.out.println("su resultado es: " + (suma1 + suma2));
+    }
+
+    static void mul() {
+        Scanner scan = new Scanner(System.in);
+        int mul1;
+        int mul2;
+        System.out.println("Usted escogio multiplación");
+        System.out.println("Escriba el primer numero");
+        mul1 = scan.nextInt();
+        System.out.println("Escribe el segundo numero");
+        mul2 = scan.nextInt();
+        System.out.println("El resultado es; " + (mul1 * mul2));
+    }
+
+    static void resta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Usted escogio RESTA");
+        System.out.println("RESTA");
+        int resta = 0;
+        int resta2 = 0;
+        System.out.println("Introduce el primer numero:");
+        resta = sc.nextInt();
+        System.out.println("Introduce el segundo numero:");
+        resta2 = sc.nextInt();
+        System.out.println("El resultado de la resta es:" + (resta - resta2));
+
+    }
+
+    static void division() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Usted escogio DIVISION");
+        System.out.println("DIVISION");
+        float division;
+        float division2;
+        System.out.println("Introduce el primero numero:");
+        division = sc.nextInt();
+        System.out.println("Introduce el segundo numero:");
+        division2 = sc.nextInt();
+        if (division2 == 0) {
+            System.out.println("no se puede dividir por 0");
+        }
+        System.out.println("El resultado de la division es:" + (division / division2));
+
+    }
+    static void primos1(){
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Usted escogio SECUENCIA DE NUMEROS PRIMOS");
+                System.out.println("NUMEROS PRIMOS");
+                int num;
+                int cont = 0;
+                int i = 2;
+                int j = 1;
+                System.out.println("Escribe el numero");
+                num = sc.nextInt();
+                while (num >= i) {
+                    while (i >= j) {
+                        if (i % j == 0) {
+                            cont++;
+                        }
+                        j++;
+                    }
+                    if (cont == 2) {
+                        System.out.println(i);
+                    }
+                    i++;
+                    cont = 0;
+                    j = 1;
+                }
+    }
+    static void fibo1(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Usted escogio SECUENCIA FIBONACCI");
+                System.out.println("FIBONACCI");
+                Scanner scan = new Scanner(System.in);
+
+                int a = 0;
+                int b = 1;
+                int c;
+                int n;
+
+                System.out.println("Escribe un numero");
+                n = scan.nextInt();
+                System.out.println(a);
+                System.out.println(b);
+                while (true) {
+                    c = a + b;
+                    if (c < n) {
+                        System.out.println(c);
+                        a = b;
+                        b = c;
+                    }
+                }
+    }
+    
 }
